5c30d9fa72dc829fb466a199142b7d171296ccb6
unknown <brettstainken.student@careerdevs.com>
2019-08-19 19:30:51 -0400

    yahtzee

--- Yahtzee.js
+++ Yahtzee.js
@@ -1,3 +1,10 @@
+const readline = require('readline');
+var keepdie = 0
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+
 function roll (){
     let x=((Math.floor(Math.random()*6))+1);
     return x;
@@ -6,7 +13,7 @@ var dicerollsfive = [];
 function yahtzee (){
     for (let i=0; i<5; i++){
         b=roll();
-        console.log(b)
+        //console.log(b)
         dicerollsfive.push(b)
     }
 }
@@ -16,5 +23,36 @@ function yahtzeethree(){
         yahtzee();
     }
 }
-console.log(yahtzeethree());
-console.log(dicerollsfive)
+function callvalue(){
+    yahtzee();
+    console.log(dicerollsfive);
+    console.log("[ 0| 1| 2| 3| 4 ]")
+    var response = rl.question('Which die are you rerolling? ', answer);
+    function answer (response){
+        var keepdie = parseFloat(response, 10);
+        console.log(dicerollsfive[keepdie])
+        rl.close();
+    }
+    return "";
+}
+function callvalues(){
+    yahtzee();
+    dicetokeep = [];
+    console.log(dicerollsfive);
+    console.log("[ 0| 1| 2| 3| 4 ]")
+    var response = rl.question('Which dice are you rerolling? ', answer);
+    function answer (response){
+        var keepdie = response.split("");
+        for (i=0; i<keepdie.length; i++){
+            let val = parseFloat(keepdie[i], 10);
+            dicetokeep.push(dicerollsfive[val])
+        }
+        console.log(dicetokeep);
+
+        rl.close();
+    }
+    return "";
+}
+
+console.log(callvalues());
+//console.log(dicerollsfive)
