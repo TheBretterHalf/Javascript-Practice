d24002df58395c70886e5cfde8b8d90bbfa852ae
unknown <brettstainken.student@careerdevs.com>
2019-08-19 18:04:40 -0400

    Yahtzee

--- Yahtzee.js
+++ Yahtzee.js
@@ -1,6 +1,20 @@
-function yahtzee (){
-    let x=Math.floor(Math.random()*6);
-    console.log(x)
+function roll (){
+    let x=((Math.floor(Math.random()*6))+1);
+    return x;
 }
-
-console.log(yahtzee)
+var dicerollsfive = [];
+function yahtzee (){
+    for (let i=0; i<5; i++){
+        b=roll();
+        console.log(b)
+        dicerollsfive.push(b)
+    }
+}
+function yahtzeethree(){
+    for (let i=0; i<3; i++){
+        dicerollsfive=[];
+        yahtzee();
+    }
+}
+console.log(yahtzeethree());
+console.log(dicerollsfive)
