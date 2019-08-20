18e4236cbe491d573044043f37f308aa6916db41
unknown <brettstainken.student@careerdevs.com>
2019-08-19 20:27:14 -0400

    readline

--- Yahtzee.js
+++ Yahtzee.js
@@ -4,12 +4,12 @@ const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
   });
-
+var dicerollsfive = [];
 function roll (){
     let x=((Math.floor(Math.random()*6))+1);
     return x;
 }
-var dicerollsfive = [];
+
 function yahtzee (){
     for (let i=0; i<5; i++){
         b=roll();
@@ -37,22 +37,32 @@ function callvalue(){
 }
 function callvalues(){
     yahtzee();
-    dicetokeep = [];
-    console.log(dicerollsfive);
-    console.log("[ 0| 1| 2| 3| 4 ]")
-    var response = rl.question('Which dice are you rerolling? ', answer);
-    function answer (response){
-        var keepdie = response.split("");
-        for (i=0; i<keepdie.length; i++){
-            let val = parseFloat(keepdie[i], 10);
-            dicetokeep.push(dicerollsfive[val])
+    dicetoreroll = [];
+    for(i=0; i<3; i++){
+        console.log(dicerollsfive);
+        console.log("[ 0| 1| 2| 3| 4 ]")
+        var response = rl.question('Which dice are you rerolling? ', answer);
+        function answer (response){
+            var rerolldie = response.split("");
+            // for (i=0; i<rerolldie.length; i++){
+            //     let val = parseFloat(rerolldie[i], 10);
+            //     dicetoreroll.push(dicerollsfive[val])
+            // }
+            //console.log(dicetoreroll);
+            for(i=0; i<rerolldie.length; i++){
+                let val = parseFloat(rerolldie[i], 10);
+                console.log(val)
+                dicerollsfive.splice(val-i,1);
+                let x=((Math.floor(Math.random()*6))+1);
+                dicerollsfive.push(x);
+                rl.close();
+            }
+            console.log(dicerollsfive)
         }
-        console.log(dicetokeep);
-
-        rl.close();
     }
     return "";
 }
+
 
 console.log(callvalues());
 //console.log(dicerollsfive)
