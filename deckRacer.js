dcdb9b8a8555b4174d7e1048916c6a83c383c14f
unknown <brettstainken.student@careerdevs.com>
2019-08-26 18:52:39 -0400

    Joe's help with deckRacer

--- deckRacer.js
+++ deckRacer.js
@@ -1,6 +1,39 @@
-var deck =
+var deck = new Array();
+var suits = ["spades", "diamonds", "clubs", "hearts"];
+var values = ["A", "2", "3", "4", "5", "6", "7"];
 
 function roll (){
     let x=((Math.floor(Math.random()*6))+1);
     return x;
 }
+
+function getDeck()
+{
+	let deck = new Array();
+	for(var i = 0; i < suits.length; i++)
+	{
+		for(var x = 0; x < values.length; x++)
+		{
+			var card = {Value: values[x], Suit: suits[i]};
+			deck.push(card);
+		}
+    }
+	return deck;
+}
+
+function shuffle()
+{
+    let deck = getDeck();
+	for (var i = 0; i < 1000; i++)
+	{
+		var location1 = Math.floor((Math.random() * deck.length));
+		var location2 = Math.floor((Math.random() * deck.length));
+		var tmp = deck[location1];
+
+		deck[location1] = deck[location2];
+		deck[location2] = tmp;
+    }
+    return deck;
+}
+
+console.log(shuffle())
