1f8693e68aeb2cdec6663db61d9ab9f778880e81
unknown <brettstainken.student@careerdevs.com>
2019-09-05 19:40:07 -0400

    interest finish

--- interest.js
+++ interest.js
@@ -2,11 +2,76 @@ var balance = 1000
 var years = 3
 var interestMonths = .45
 
+
 function nB (){
     let x = Math.floor(1000*(Math.pow(1+((interestMonths)/(12)), 12*years)))
     console.log(x)
 }
 
-function nBperMonth
+function nBperMonth (){
+    let monthlybal = balance;
+    for (let i=0; i<years*12; i++){
+        let monthlyx = monthlybal*((interestMonths)/12)
+        monthlybal=monthlybal+monthlyx;
+        console.log(Math.floor(monthlybal));
+    }
+}
 
-console.log(nB())
+function deposit (deposit){
+    let monthlybal = balance;
+    for (let i=0; i<years*12; i++){
+        let monthlyx = monthlybal*((interestMonths)/12)
+        monthlybal=monthlybal+monthlyx+deposit;
+        console.log(Math.floor(monthlybal));
+    }
+}
+
+function depositMonthly (deposit, month){
+    let monthlybal = balance;
+    for (let i=0; i<years*12; i++){
+        if(i%month==0){
+            let monthlyx = monthlybal*((interestMonths)/12)
+            monthlybal=monthlybal+monthlyx+deposit;
+            console.log(Math.floor(monthlybal));
+        }
+        else{
+            let monthlyx = monthlybal*((interestMonths)/12)
+            monthlybal=monthlybal+monthlyx;
+            console.log(Math.floor(monthlybal));
+        }
+    }
+}
+
+function withdrawl (deposit, month, withdrawl, monthW){
+    let monthlybal = balance;
+    for (let i=0; i<years*12; i++){
+        if(i%month==0){
+            if(i%monthW==0){
+                monthlybal=monthlybal-withdrawl
+                let monthlyx = monthlybal*((interestMonths)/12)
+                monthlybal=monthlybal+monthlyx+deposit;
+                console.log(Math.floor(monthlybal));
+            }
+            else{
+                let monthlyx = monthlybal*((interestMonths)/12)
+                monthlybal=monthlybal+monthlyx+deposit;
+                console.log(Math.floor(monthlybal));
+            }
+        }
+        else{
+            if(i%monthW==0){
+                monthlybal=monthlybal-withdrawl
+                let monthlyx = monthlybal*((interestMonths)/12)
+                monthlybal=monthlybal+monthlyx;
+                console.log(Math.floor(monthlybal));
+            }
+            else{
+                let monthlyx = monthlybal*((interestMonths)/12)
+                monthlybal=monthlybal+monthlyx;
+                console.log(Math.floor(monthlybal));
+            }
+        }
+    }
+}
+
+console.log(depositMonthly(100,4,100,4))
